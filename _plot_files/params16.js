61b4d755e6ebf5082d0ba3271236736b98599431
wdomitrz <witekdomitrz@gmail.com>
2020-04-23 01:21:20 +0200

    Explanations with plots

---
+++ _plot_files/params16.js
@@ -0,0 +1,21 @@
+params = {
+  "variables": [
+    "previous_bookings_not_canceled",
+    "previous_cancellations",
+    "lead_time",
+    "customer_type_Group"
+  ],
+  "color": null,
+  "show_profiles": true,
+  "show_observations": true,
+  "show_rugs": false,
+  "show_residuals": false,
+  "add_table": true,
+  "height": 500,
+  "width": 600,
+  "plot_title": "",
+  "size_ices": 2,
+  "alpha_ices": 0.4,
+  "yaxis_title": "y",
+  "aggregate_profiles": null
+};
